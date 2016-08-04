f935ebd28964d8f236d833a96256c923baddbbe4
Aborilov Pavel <paborilov@cloudlinux.com>
2016-08-04 14:03:52 +0300

    AC-4120: create localstorages and apply fslimits
    Change-Id: I554998408316f5be1c894d4feafd5dde9b25afbc

--- pkg/kubelet/kdplugins/kdhookplugin.go
+++ pkg/kubelet/kdplugins/kdhookplugin.go
@@ -1,6 +1,11 @@
 package kdplugins
 
 import (
+	"encoding/json"
+	"os"
+	"os/exec"
+	"strconv"
+
 	"github.com/golang/glog"
 	"k8s.io/kubernetes/pkg/api"
 )
@@ -9,11 +14,91 @@ type KDHookPlugin struct {
 }
 
 func (p *KDHookPlugin) OnContainerCreatedInPod(container *api.Container, pod *api.Pod) {
-	glog.V(4).Infof(">>>>>>>>>>> Container %q created in pod %q", container.Name, pod.Name)
+	glog.V(4).Infof(">>>>>>>>>>> Container %q created in pod! %q", container.Name, pod.Name)
+}
+
+const fsLimitPath string = "/var/lib/kuberdock/scripts/fslimit.py"
+
+type volumeSpec struct {
+	Path string  `json:"path"`
+	Name string  `json:"name"`
+	Size float64 `json:"size"`
+}
+
+type volumeAnnotation struct {
+	LocalStorage *volumeSpec `json:"localStorage,omitempty"`
+}
+
+func getVolumeSpecs(pod *api.Pod) []volumeSpec {
+	if va, ok := pod.Annotations["kuberdock-volume-annotations"]; ok {
+		var data []volumeAnnotation
+		if err := json.Unmarshal([]byte(va), &data); err != nil {
+			glog.V(4).Infof("Error while try to parse json(%s): %q", va, err)
+			return nil
+		} else {
+			specs := make([]volumeSpec, 0, len(data))
+			for _, volume := range data {
+				if volume.LocalStorage != nil && (volume.LocalStorage.Path != "" && volume.LocalStorage.Name != "") {
+					if volume.LocalStorage.Size == 0 {
+						volume.LocalStorage.Size = 1
+					}
+					specs = append(specs, *volume.LocalStorage)
+				}
+			}
+			return specs
+		}
+	}
+	return nil
 }
 
 func (p *KDHookPlugin) OnPodRun(pod *api.Pod) {
-	glog.V(4).Infof(">>>>>>>>>>> Pod %q run", pod.Name)
+	glog.V(4).Infof(">>>>>>>>>>> Pod %q run!", pod.Name)
+	if specs := getVolumeSpecs(pod); specs != nil {
+		ProcessLocalStorages(specs)
+	}
+}
+
+// Process all needed operations with localstorages,
+// like creating directories, apply quota, restore from backup, etc.
+// Parse json volumeAnnotation from Pod Annotation field kuberdock-volume-annotations.
+func ProcessLocalStorages(specs []volumeSpec) {
+	for _, spec := range specs {
+		if err := createVolume(spec); err != nil {
+			continue
+		}
+		if err := applyFSLimits(spec); err != nil {
+			continue
+		}
+	}
+}
+
+// Create all necessary directories with needed permissions
+// and securety context.
+// Return error as nil if has no problem
+// or return error.
+func createVolume(spec volumeSpec) error {
+	if err := os.MkdirAll(spec.Path, 0755); err != nil {
+		glog.V(4).Infof("Error, while mkdir: %q", err)
+		return err
+	}
+	err := exec.Command("chcon", "-Rt", "svirt_sandbox_file_t", spec.Path).Run()
+	if err != nil {
+		glog.V(4).Infof("Error, while chcon: %q", err)
+		return err
+	}
+	return nil
+}
+
+// Apply quota to path with size in Gb.
+// Return error as nil if has no problem or
+// return error.
+func applyFSLimits(spec volumeSpec) error {
+	err := exec.Command("/usr/bin/env", "python2", fsLimitPath, "storage", spec.Path+"="+strconv.Itoa(int(spec.Size))+"g").Run()
+	if err != nil {
+		glog.V(4).Infof("Error, while call fslimit: %q\n", err)
+		return err
+	}
+	return nil
 }
 
 func (p *KDHookPlugin) OnPodKilled(pod *api.Pod) {

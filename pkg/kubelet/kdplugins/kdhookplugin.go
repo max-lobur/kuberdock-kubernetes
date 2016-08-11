b487374e092616f24eac22d2bef81c8d29750b9c
Aborilov Pavel <paborilov@cloudlinux.com>
2016-08-11 07:54:36 +0300

    AC-4121: publicIP logic moved to kubelet plugin
    Change-Id: I336f17ff2f872d0d13ba06d6bb5a44abd198f6e8

--- pkg/kubelet/kdplugins/kdhookplugin.go
+++ pkg/kubelet/kdplugins/kdhookplugin.go
@@ -2,9 +2,11 @@ package kdplugins
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
 
 	"github.com/golang/glog"
 	"k8s.io/kubernetes/pkg/api"
@@ -29,6 +31,8 @@ type volumeAnnotation struct {
 	LocalStorage *volumeSpec `json:"localStorage,omitempty"`
 }
 
+// Get localstorage volumes spec from pod annotation
+// Return list of volumeSpec or nil, if no any.
 func getVolumeSpecs(pod *api.Pod) []volumeSpec {
 	if va, ok := pod.Annotations["kuberdock-volume-annotations"]; ok {
 		var data []volumeAnnotation
@@ -51,17 +55,66 @@ func getVolumeSpecs(pod *api.Pod) []volumeSpec {
 	return nil
 }
 
+// Get publicIP from pod labels
+// Return publicIP as string or empty string
+func getPublicIP(pod *api.Pod) string {
+	if publicIP, ok := pod.Labels["kuberdock-public-ip"]; ok {
+		return publicIP
+	}
+	return ""
+}
+
 func (p *KDHookPlugin) OnPodRun(pod *api.Pod) {
 	glog.V(4).Infof(">>>>>>>>>>> Pod %q run!", pod.Name)
 	if specs := getVolumeSpecs(pod); specs != nil {
-		ProcessLocalStorages(specs)
+		processLocalStorages(specs)
+	}
+	if publicIP := getPublicIP(pod); publicIP != "" {
+		handlePublicIP("add", publicIP)
+	}
+}
+
+// Get network interface, where we need to add publicIP.
+// Return network interface name as string and error as nil
+// or empty string with error if can't get one.
+func getIFace() (string, error) {
+	// TODO: find the better way to get flannel network interface
+	out, err := exec.Command("bash", "-c", "source /etc/sysconfig/flanneld && echo $FLANNEL_OPTIONS").CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("Error while get iface from %s", out)
+	}
+	if l := strings.Split(string(out), "="); len(l) == 2 {
+		iface := l[1]
+		return strings.TrimSpace(iface), nil
+	}
+	return "", fmt.Errorf("Error while get iface from %s", out)
+}
+
+// Add or delete publicIP on network interface depending on action.
+// Action can be add or del strings.
+func handlePublicIP(action string, publicIP string) {
+	iface, err := getIFace()
+	if err != nil {
+		glog.V(4).Info(err)
+		return
+	}
+	out, err := exec.Command("ip", "addr", action, publicIP+"/32", "dev", iface).CombinedOutput()
+	if err != nil {
+		glog.V(4).Infof("Error while try to %s publicIP(%s): %q, %s", action, publicIP, err, out)
+		return
+	}
+	if action == "add" {
+		out, err := exec.Command("arping", "-I", iface, "-A", publicIP, "-c", "10", "-w", "1").CombinedOutput()
+		if err != nil {
+			glog.V(4).Infof("Error while try to arping: %q:%s", err, out)
+		}
 	}
 }
 
 // Process all needed operations with localstorages,
 // like creating directories, apply quota, restore from backup, etc.
 // Parse json volumeAnnotation from Pod Annotation field kuberdock-volume-annotations.
-func ProcessLocalStorages(specs []volumeSpec) {
+func processLocalStorages(specs []volumeSpec) {
 	for _, spec := range specs {
 		if err := createVolume(spec); err != nil {
 			continue
@@ -104,5 +157,8 @@ func applyFSLimits(spec volumeSpec) error {
 func (p *KDHookPlugin) OnPodKilled(pod *api.Pod) {
 	if pod != nil {
 		glog.V(4).Infof(">>>>>>>>>>> Pod %q killed", pod.Name)
+		if publicIP := getPublicIP(pod); publicIP != "" {
+			handlePublicIP("add", publicIP)
+		}
 	}
 }
